a776a53dd85e78edef72b5a77bc0fb9821a8b527
petkov001 <petkov001@yahoo.co.uk>
2016-04-24 16:22:59 +0100

    add bootstrap and angular js

---
+++ app/js/controllers.js
@@ -0,0 +1,21 @@
+var shopApp = angular.module('shopApp', []);
+
+shopApp.controller('ProductListCtrl', function ($scope) {
+	$scope.products = [
+		{
+			name: 'phone',
+			description: 'Samsung S6',
+			price: 325.99
+		},
+		{
+			name: 'phone',
+			description: 'Samsung S4',
+			price: 125.99
+		},
+		{
+			name: 'phone',
+			description: 'Samsung A5',
+			price: 225.99
+		}
+	];
+});
